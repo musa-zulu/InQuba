7e76f33cb3ede6562828655c3f4e490adb385a5c
Musa <zuluchs@gmail.com>
2021-11-28 13:34:03 +0200

    Create SugarTests.cs

---
+++ source/CoffeeMachine.Tests/Logic/SugarTests.cs
@@ -0,0 +1,46 @@
+﻿using CoffeeMachine.Domain;
+using NUnit.Framework;
+
+namespace CoffeeMachine.Tests.Logic
+{
+    [TestFixture]
+    public class SugarTests
+    {
+
+        [Test]
+        public void Construct()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            Assert.DoesNotThrow(() => new Sugar(1));
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void Construct_ShouldSetIngredientNameToSugar()
+        {
+            //---------------Set up test pack-------------------
+            var sut = new Sugar(1);
+            var expected = "Sugar";
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var results = sut.IngredientName;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, results);
+        }
+
+        [Test]
+        public void Construct_ShouldSetIngredientUnitToOne()
+        {
+            //---------------Set up test pack-------------------
+            var ingredientsUnit = 1;
+            var sut = new Sugar(ingredientsUnit);
+            //---------------Assert Precondition----------------
+            //---------------Execute Test ----------------------
+            var results = sut.IngredientUnit;
+            //---------------Test Result -----------------------
+            Assert.AreEqual(ingredientsUnit, results);
+        }
+    }
+}
